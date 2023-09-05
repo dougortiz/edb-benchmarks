784b3259b60cb7c5634ea9bf7c7f6460c87ce2ca
dougortiz <doug.ortiz@enterprisedb.com>
2023-09-05 11:38:14 -0600

    Benchmark issues

--- aws-pgd-3-nodes-camo/04_execute/run.sh
+++ aws-pgd-3-nodes-camo/04_execute/run.sh
@@ -10,17 +10,13 @@ export ANSIBLE_SSH_ARGS="-o ForwardX11=no -o UserKnownHostsFile=/dev/null"
 export ANSIBLE_SSH_PIPELINING=true
 export ANSIBLE_HOST_KEY_CHECKING=false
 
-# # Run the ramping up benchmark
-# ansible-playbook \
-#     -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
-#     -e "@$SOURCEDIR/../environment.yml" \
-#     -e "@$SOURCEDIR/../vars.yml" \
-#     "${SOURCEDIR}/playbook-setup-sync-repl.yml"
-
 ansible-playbook \
     -i "${TERRAFORM_PROJECT_PATH}/inventory.yml" \
     -e "@$SOURCEDIR/../environment.yml" \
     -e "@$SOURCEDIR/../vars.yml" \
     -e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+    -e "pgd_bench_duration=${PGD_BENCH_DURATION}" \
+    -e "pgd_bench_scale_factor=${PGD_BENCH_SCALE_FACTOR}" \
+    -e "pgd_bench_clients=${PGD_BENCH_CLIENTS}" \
     -e "results_directory=${RESULTS_DIRECTORY}/report-data" \
     "${SOURCEDIR}/playbook-pgdbench-run.yml"
--- aws-pgd-3-nodes-camo/environment.sh
+++ aws-pgd-3-nodes-camo/environment.sh
@@ -7,35 +7,32 @@ export BENCHMARK_NAME="${BENCHMARK_NAME:=AWS_PGD_3_NODES}"
 export ANSIBLE_VERBOSITY="${ANSIBLE_VERBOSITY:=0}"
 
 # Credentials
-#export REPO_TOKEN="${REPO_TOKEN:=<secret>}"
-export REPO_TOKEN="${REPO_TOKEN:=pdZe6pcnWIgmuqdR7v1L38rG6Z6wJEsY}"
+export REPO_TOKEN="${REPO_TOKEN:=<secret>}"
 
 # Terraform
 export REGION="${REGION:=us-east-2}"
 export ZONE0="${ZONE0:=us-east-2a}"
 export ZONE1="${ZONE1:=us-east-2b}"
 export ZONE2="${ZONE2:=us-east-2c}"
+
 # Benchmarker instance
 export DRIVER_INSTANCE_TYPE="${DRIVER_INSTANCE_TYPE:=c5d.2xlarge}"
+
 # Postgres Instance
 export PG_INSTANCE_TYPE="${PG_INSTANCE_TYPE:=c5d.2xlarge}" # Assumes attached storage devices from instance type
 export PG_IMAGE_NAME="${PG_IMAGE_NAME:=Rocky-8-ec2-8.6-20220515.0.x86_64}"
 export PG_IMAGE_OWNER="${PG_IMAGE_OWNER:=679593333241}"
 export PG_SSH_USER="${PG_SSH_USER:=rocky}"
 
-# # TPCC
-# export TPCC_DURATION="${TPCC_DURATION:=20}"
-# export TPCC_WAREHOUSE="${TPCC_WAREHOUSE:=1000}"
-# export TPCC_RAMPUP="${TPCC_RAMPUP:=1}"
-# export TPCC_LOADER_VUSERS="${TPCC_LOADER_VUSERS:=75}"
-# export TPCC_MIN_VUSERS="${TPCC_MIN_VUSERS:=5}"
-# export TPCC_MAX_VUSERS="${TPCC_MAX_VUSERS:=100}"
-# export TPCC_STEP_VUSERS="${TPCC_STEP_VUSERS:=5}"
+# CAMO pgd_bench configuration settings
+export PGD_BENCH_DURATION="${PGD_BENCH_DURATION:=15}"
+export PGD_BENCH_SCALE_FACTOR="${PGD_BENCH_SCALE_FACTOR:=10}"
+export PGD_BENCH_CLIENTS="${PGD_BENCH_CLIENTS:=4}"
 
-# # Storage
-# export PGDATA_STORAGE_TYPE="${STORAGE_TYPE:=io2}"
-# export PGDATA_STORAGE_SIZE="${STORAGE_SIZE:=4096}"
-# export PGDATA_STORAGE_IOPS="${STORAGE_IOPS:=21667}"
-# export PGWAL_STORAGE_TYPE="${STORAGE_TYPE:=io2}"
-# export PGWAL_STORAGE_SIZE="${STORAGE_SIZE:=4096}"
-# export PGWAL_STORAGE_IOPS="${STORAGE_IOPS:=21667}"
+# Storage
+export PGDATA_STORAGE_TYPE="${STORAGE_TYPE:=io2}"
+export PGDATA_STORAGE_SIZE="${STORAGE_SIZE:=4096}"
+export PGDATA_STORAGE_IOPS="${STORAGE_IOPS:=21667}"
+export PGWAL_STORAGE_TYPE="${STORAGE_TYPE:=io2}"
+export PGWAL_STORAGE_SIZE="${STORAGE_SIZE:=4096}"
+export PGWAL_STORAGE_IOPS="${STORAGE_IOPS:=21667}"
